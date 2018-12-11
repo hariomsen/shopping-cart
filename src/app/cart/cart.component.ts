22f3b133460ed7a9adead4e9706d590e87926170
sudikm <sudik.maharana@wwindia.com>
2018-12-11 19:02:17 +0530

    cart to invoice work

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service'
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -11,7 +12,8 @@ import { CartService } from '../cart.service'
 export class CartComponent implements OnInit {
   cartList: any[];
   total:any={};
-  constructor() {
+  closeResult: string;
+  constructor(private modalService: NgbModal) {
     this.cartList = CartService.cartList;
     this.total = CartService.total
   }
@@ -32,5 +34,20 @@ export class CartComponent implements OnInit {
     item.count = item.count - 1;
     this.setFinalAmout();
   }
-
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
 }
